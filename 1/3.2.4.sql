746bba5e1e98516a14eb896e0aab231a5aed157d
懒得取名字了 <44843937+mhq1065@users.noreply.github.com>
2021-04-08 14:57:34 +0800

    Update 3.2.4.sql

--- 1/3.2.4.sql
+++ 1/3.2.4.sql
@@ -1,7 +1,8 @@
 -- 4
 SELECT `S#`,Sname,FLOOR(DATEDIFF(CURDATE(), Bdate)/365.2422) 
 FROM Student
-WHERE Height > (
+WHERE sex="男" 
+    AND Height > ALL(
         SELECT Height
         FROM student
         WHERE Sname = '王涛'
